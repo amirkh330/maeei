b6c8b039eb65e4304497722d3ef589e8452e70ef
amirkh330 <amir73khakbaz@gmail.com>
2024-05-03 22:56:13 +0330

    first test for pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
   return (
     <html>
       <body dir="rtl">
+      <script src="/service-worker.js" defer></script>
           <ChakraProvider theme={ChakraTheme}>
             <NavBarLayout>{children}</NavBarLayout>
           </ChakraProvider>
